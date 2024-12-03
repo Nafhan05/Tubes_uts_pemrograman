b899068f9bcc4e752e78fa176bb13a6854a9dfb9
Nafhan05 <16523213@std.stei.itb.ac.id>
2024-12-03 08:56:17 +0700

    fix bug

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", 5000))  # Default port 5000 jika tidak ada di env
-    socketio.run(app, debug=True, host="0.0.0.0", port=os.getenv("PORT", 5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))  # Menambahkan host dan port untuk akses lebih fleksibel
+    socketio.run(app, debug=True)
