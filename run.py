b0dcf475605301659fe3af589927b6612a35b977
Nafhan05 <16523213@std.stei.itb.ac.id>
2024-12-03 08:06:51 +0700

    refactor: run and gunicorn

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))  # Menambahkan host dan port untuk akses lebih fleksibel
-    socketio.run(app, debug=True)
+    # app.run(debug=True, port=os.getenv("PORT", default=5000))  # Menambahkan host dan port untuk akses lebih fleksibel
+    socketio.run(app, debug=True, port=os.getenv("PORT", default=5000)) 
